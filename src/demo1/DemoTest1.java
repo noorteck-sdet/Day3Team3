befe66ced685456d57afa0d142f898cc39fe90c9
A U Thor <eu.dawran@gmail.com>
2024-01-09 18:41:29 -0500

    implemented add()

--- src/demo1/DemoTest1.java
+++ src/demo1/DemoTest1.java
@@ -8,36 +8,34 @@ public class DemoTest1 {
 		System.out.println("Team 3");
 		System.out.println("Alishan stop...");
 
-		//bye Alishan
-		
-		//Ahmed Aly Push
-	    ArrayList<Integer> numList = new ArrayList<Integer>();
-		
-			numList.add(2);
-			numList.add(5);
-			numList.add(6);
-			numList.add(23);
-			numList.add(656);
-			numList.add(22);
-			numList.add(33);
-			numList.add(22);
-			numList.add(33);
-			numList.add(1);
-			
-			
-			
-			
-			for (Integer num : numList) {
-				System.out.println(num);
-		
-	}
-	
-	
-	
-	//WE LOVE JAVA!!!!!! - said no one
+		// bye Alishan
 
-	
-	}	
-	
-	//Brian
+		// Ahmed Aly Push
+		ArrayList<Integer> numList = new ArrayList<Integer>();
+
+		numList.add(2);
+		numList.add(5);
+		numList.add(6);
+		numList.add(23);
+		numList.add(656);
+		numList.add(22);
+		numList.add(33);
+		numList.add(22);
+		numList.add(33);
+		numList.add(1);
+
+		for (Integer num : numList) {
+			System.out.println(num);
+
+		}
+
+		// Brian
+	}
+
+	public void add(int a, int b) {
+		System.out.println(a + b);
+	}
+
+	// WE LOVE JAVA!!!!!! - said no one
+
 }
