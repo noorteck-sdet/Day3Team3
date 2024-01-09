cb21fc319b4c6ac6b56f458c3125507017c43112
Kamy <Kamy@4Ever.mynetworksettings.com>
2024-01-08 19:59:06 -0500

    Printing

--- src/demo1/DemoTest1.java
+++ src/demo1/DemoTest1.java
@@ -4,7 +4,7 @@ public class DemoTest1 {
 	
 	public static void main(String[] args) {
 		System.out.println("Team 3");
-		System.out.println("Happy bday Alishan...");
+		System.out.println("Alishan stop...");
 	}
 	
 
